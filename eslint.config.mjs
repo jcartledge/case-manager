4d5fab8dded1bcf6189a6a0f52ebc5b8a3d9ba99
James Cartledge <jcartledge@gmail.com>
2024-08-17 22:47:27 +1000

    chore: eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,23 @@ import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
 export default [
-    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    ...compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ),
     {
         plugins: {
             "@typescript-eslint": typescriptEslint,
@@ -24,4 +28,5 @@ export default [
             parser: tsParser,
         },
     },
+    eslintConfigPrettier,
 ];
